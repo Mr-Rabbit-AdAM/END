e8dc5479377dfa097e1e679b541a6d68beb17195
Ad-AM <87221134+Mr-Rabbit-AdAM@users.noreply.github.com>
2021-12-01 11:29:28 +0630

    Create end.sh

---
+++ end.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+clear 
+echo -e "\e[5;95m          
+                             ( \---/ )
+                              ) . . (
+________________________,--._(___Y___)_,--._______________________ 
+                         --'            --' \e[0m"
+
+
+
+ echo -e "\e[1;32m                   ##########BY A d A M##########"
+ echo ""
+ echo -e "\e[2;33m         ') This is my last greeting. Script I wish you all the best \e[0"
+ echo ""
+ echo "1.web-try"
+ echo ""
+ echo "2.web-for-pentest"
+ echo ""
+ echo "3.hash-Crack"
+ echo ""
+ echo "4.Network-Scan"
+ echo ""
+ echo "5.bule-god"
+ echo ""
+ echo "6.ip-info"
+ echo ""
+ echo "7.track"
+ echo ""
+ echo "8.Nethunter-Vnc-error-clear"
+ echo ""
+ echo "9.BASH-CHAT"
+ echo ""
+ echo "10.Deface-method-webdav"
+ echo ""
+ echo "11.Termux-games"
+ echo ""
+ echo "12.Termux-bomb"
+ echo ""
+ echo "13.Fire-Ghost"
+ echo ""
+ echo "14.pornhub"
+ echo ""
+ echo "15.files-upload"
+ echo ""
+ read -p "END@BYADAM :: " g  
+case g in 
+1) 
+echo -e "\e[1;33m  Please  Download Start "
+git clone https://github.com/Mr-Rabbit-AdAM/web-try 
+echo -e "\e[1;32m Done \0m"
+;;
+2)
+echo -e "\e[1;33m  Please  Download Start "
+git clone https://github.com/Mr-Rabbit-AdAM/web-for-pentest
+echo -e "\e[1;32m Done \0m"
+;;
+3)
+echo -e "\e[1;33m  Please  Download Start "
+git clone https://github.com/Mr-Rabbit-AdAM/hash-Crack
+echo -e "\e[1;32m Done \0m"
+;;
+4)
+echo -e "\e[1;33m  Please  Download Start "
+git clone https://github.com/Mr-Rabbit-AdAM/Network-Scan 
+echo -e "\e[1;32m Done \0m"
+;;
+5) 
+echo -e "\e[1;33m  Please  Download Start "
+git clonehttps://github.com/Mr-Rabbit-AdAM/bule-god
+echo -e "\e[1;32m Done \0m"
+;;
+6)
+echo -e "\e[1;33m  Please  Download Start "
+git clone https://github.com/Mr-Rabbit-AdAM/ip-info
+echo -e "\e[1;32m Done \0m"
+;;
+7)
+echo -e "\e[1;33m  Please  Download Start "
+git clone https://github.com/Mr-Rabbit-AdAM/track
+echo -e "\e[1;32m Done \0m"
+;;
+8)
+echo -e "\e[1;33m  Please  Download Start "
+git clone https://github.com/Mr-Rabbit-AdAM/Nethunter-Vnc-error-clear
+echo -e "\e[1;32m Done \0m"
+;;
+9)
+echo -e "\e[1;33m  Please  Download Start "
+git clone https://github.com/Mr-Rabbit-AdAM/BASH-CHAT
+echo -e "\e[1;32m Done \0m"
+;;
+10)
+echo -e "\e[1;33m  Please  Download Start "
+git clone https://github.com/Mr-Rabbit-AdAM/Deface-method-webdav
+echo -e "\e[1;32m Done \0m"
+;;
+11)
+echo -e "\e[1;33m  Please  Download Start "
+git clone https://github.com/Mr-Rabbit-AdAM/Termux-games
+echo -e "\e[1;32m Done \0m"
+;;
+12)
+echo -e "\e[1;33m  Please  Download Start "
+git clone https://github.com/Mr-Rabbit-AdAM/Termux-bomb
+echo -e "\e[1;32m Done \0m"
+;;
+13)
+echo -e "\e[1;33m  Please  Download Start "
+git clone https://github.com/Mr-Rabbit-AdAM/Fire-Ghost
+echo -e "\e[1;32m Done \0m"
+;;
+14)
+echo -e "\e[1;33m  Please  Download Start "
+git clone https://github.com/Mr-Rabbit-AdAM/pornhub
+echo -e "\e[1;32m Done \0m"
+;;
+15)
+echo -e "\e[1;33m  Please  Download Start "
+git clone https://github.com/Mr-Rabbit-AdAM/files-upload
+echo -e "\e[1;32m Done \0m"
+;;
+*)
+echo -e "\e[1;33 Wrong choice\e[0m"
+esac
